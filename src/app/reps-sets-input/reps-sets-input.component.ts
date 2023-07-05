37403dcc3c5c8c6b53a99eb87e9b69abfecb0135
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-05 10:39:50 +0200

    Feature: Implementation of routing and an AuthGaurd

---
+++ src/app/reps-sets-input/reps-sets-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reps-sets-input',
+  templateUrl: './reps-sets-input.component.html',
+  styleUrls: ['./reps-sets-input.component.scss']
+})
+export class RepsSetsInputComponent {
+
+}

37e592fa72bc3128807540e4bcbe908e25411981
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-06 11:17:37 +0200

    Feature: Main implementation of the DesktopMenu with icons.

--- src/app/components/desktop-menu/desktop-menu.component.ts
+++ src/app/components/desktop-menu/desktop-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-desktop-menu',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./desktop-menu.component.scss']
 })
 export class DesktopMenuComponent {
-
+  constructor(protected router: Router) { }
 }
+
+
+
+
